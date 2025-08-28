2c42bf512263aa065b940e0f17bf147bcf835cff
jeeva-004 <mjeevanantham004@gmail.com>
2025-08-28 15:38:45 +0530

    all design fixed

---
+++ script/script-4.js
@@ -0,0 +1,6 @@
+const btn = document.querySelector('.confirm-btn');
+btn.addEventListener('click', ()=>{
+    document.querySelector('.step-4').style.display='none';
+    document.querySelector('.step-5').style.display='flex';
+})
+
